5e34651f3d88e7b51eaac21150db1e31b36808a8
LukePower48 <20.luke.power@msltcastlegregory.ie>
2024-09-26 11:02:15 +0100

    Create loops2.py

---
+++ loops2.py
@@ -0,0 +1,41 @@
+# number = 1000
+# while number >= 1000 and number <= 1500:
+#     print (number)
+#     number +=1
+
+
+# one = int(input("Enter the first number...."))
+# two = int(input("Enter the next number...."))
+# three = int(input("Enter the next number...."))
+# four = int(input("Enter the next number...."))
+# five = int(input("Enter the next number...."))
+# six = int(input("Enter the next number...."))
+# print ("your average is --> ",(one + two + three + four + five + six)// 6)
+
+# name = "Maureen"
+# user_input = input("please enter your username  ")
+# if user_input == name:
+#     password = "123abc"
+#     password_input = input("please enter a password  ")
+# while password_input == password and name == user_input:
+#     print ("Correct!")
+#     break
+# else:
+#     print ("Access denied!")
+
+
+# number = int(input("Please enter a limit")
+
+
+# number = int(input("Please enter a number, enter 0 to finish")
+# while number >0:
+#              number = int(input("Please enter a number, enter 0 to finish")
+# elif number = 0:
+#                print (number)
+
+string = input ("please enter a string ")
+spacecount = string.count(" ")
+print (spacecount)
+wordcount = string.count(
+total = wordcount-spacecount
+print (total)
