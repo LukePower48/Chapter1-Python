0c68980ea22d0fb7a0663fe2e87425db6ed9721b
LukePower48 <20.luke.power@msltcastlegregory.ie>
2024-10-23 18:26:55 +0100

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,86 @@
+"""Challenge 1"""
+# age = input("Please enter your age: ")
+# print ("Your age is:",age)
+#_____________________________________________________________________
+"""Challenge 5"""
+# name = input("What is your name? ")
+# subject = input("What is your favorite subject " + name +"? ")
+# print ("I also like",subject)
+#____________________________________________________________________-
+"""Challenge 8"""
+# marks = int(input("Enter your marks here: "))
+# if marks in range (75,101):
+#     print ("You got an A")
+# if marks in range (60,75):
+#     print ("You got a B")
+# if marks in range (35,60):
+#     print ("You got a C")
+# if marks < 35:
+#     print ("You got a D")
+#___________________________________________________________________________________________________________________
+"""Challenge 9"""
+# # user_input = input("Enter one of the olympic values: ")
+# # if user_input == "Respect" or user_input =="Excellence" or user_input =="Friendship":
+# #     print ("Correct!")
+# # else:
+# #     print ("Try again.....")
+# ________________________________________________________________________________________________________________________
+"""Challenge 10"""
+# import random
+# 
+# def get_computer_choice():
+#     choices = ['rock', 'paper', 'scissors']
+#     return random.choice(choices)
+# def get_user_choice():
+#     user_input = input("Please enter your choice, rock, paper or scissors: ")
+#     return user_input
+# def winner(user_choice,computer_choice):
+#     if user_choice == computer_choice:
+#         print ("Its a tie!")
+#     elif (user_choice == 'rock' and computer_choice == 'scissors'):
+#         print ("You win!")
+#     elif   (user_choice == 'paper' and computer_choice == 'rock'):
+#         print ("You win!")
+#     elif (user_choice == 'scissors' and computer_choice == 'paper'):
+#         print ("You win!")
+#     else:
+#         print ("You lose.... :(")
+#         
+# def play_game():
+#     user_choice = get_user_choice()
+#     computer_choice = get_computer_choice()
+#     print ("You chose...",user_choice)
+#     print ("Opponent chose...",computer_choice)
+#     result = winner(user_choice, computer_choice)
+#     print (result)
+# a = (2)
+# if a == (2):
+#     play_game()
+#_____________________________________________________________________________
+"""Challenge 11"""
+# user_input = input("Please enter a sentence: ")
+# length = len(user_input)
+# print ("Number of characters is...",length)
+# _______________________________________________________
+"""Challenge 12"""
+# user_input = input("Enter a lowercase sentence ")
+# print (user_input.upper())
+# ___________________________________________________
+"""Challenge 15"""
+# user_input = input("Enter a uppercase sentence ")
+# print (user_input.lower())
+# _____________________________________________________
+"""Challenge 16"""
+# counter = int(0)
+# for i in range(10):
+#     counter +=1
+#     print (counter)
+# ____________________________________________
+"""Challenge 18"""
+# counter = int(0)
+# number = int(input ("Enter a number "))
+# for i in range(12):
+#     counter += 1
+#     table = (number*counter)
+#     print (table)
+# ____________________________________________
