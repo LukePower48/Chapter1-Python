cf937cc6b39332f69aa21672499f6a9f96d2d38e
LukePower48 <20.luke.power@msltcastlegregory.ie>
2024-10-24 14:09:47 +0100

    Create rockpaperscissors.py

---
+++ rockpaperscissors.py
@@ -0,0 +1,30 @@
+# import random
+# 
+# def get_computer_choice():
+#     choices = ['rock', 'paper', 'scissors']
+#     return random.choice(choices)
+# def get_user_choice():
+#     user_input = input("Please enter your choice, rock, paper or scissors: ")
+#     return user_input
+# def winner(user_choice,computer_choice):
+#     if user_choice == computer_choice:
+#         print ("Its a tie!")
+#     elif (user_choice == 'rock' and computer_choice == 'scissors'):
+#         print ("You win!")
+#     elif   (user_choice == 'paper' and computer_choice == 'rock'):
+#         print ("You win!")
+#     elif (user_choice == 'scissors' and computer_choice == 'paper'):
+#         print ("You win!")
+#     else:
+#         print ("You lose.... :(")
+#         
+# def play_game():
+#     user_choice = get_user_choice()
+#     computer_choice = get_computer_choice()
+#     print ("You chose...",user_choice)
+#     print ("Opponent chose...",computer_choice)
+#     result = winner(user_choice, computer_choice)
+#     print (result)
+# a = (2)
+# if a == (2):
+#     play_game()
